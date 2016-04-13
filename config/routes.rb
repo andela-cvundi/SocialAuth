8a1b268e889833b2cb154054be90e41d842bdd40
andela-cvundi <christophervundi@gmail.com>
2016-04-13 15:51:46 +0300

    Add route for auth failures

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
     root 'pages#index'
     get '/auth/:provider/callback', to: 'sessions#create'
+    get '/auth/failure', to: 'sessions#auth_failure'
     delete '/logout', to: 'sessions#destroy'
 end
