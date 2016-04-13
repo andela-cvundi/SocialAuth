775bc58fd07ae13e665988911d55042edf051393
andela-cvundi <christophervundi@gmail.com>
2016-04-13 10:22:18 +0300

    Add a logout link

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
     root 'pages#index'
     get '/auth/:provider/callback', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy', as: 'logout_path'
 end
